31249067322bc79b08c9bc330fa3fe9d3b55b547
dllo <dllo@yuxiaomin.local>
2016-08-29 19:22:48 +0800

    热点布局

---
+++ End/app/src/main/java/com/example/dllo/end/Hotest.java
@@ -0,0 +1,18 @@
+package com.example.dllo.end;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dllo on 16/8/29.
+ */
+public class Hotest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //热点布局
+    }
+}
